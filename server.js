cf0bf35677e06f1473cb45dab832c13b19baa8c8
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-19 16:13:42 +0100

    test-heroku-deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/data-visualization-front-end'));
+app.use(express.static('./dist/data-visualization'));
 
 app.get('/*', function(req,res) {
 
