f136db34058059466a8a05d85dea1de807253665
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-20 16:11:26 +0100

    GET requests to jsonplaceholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Data Visualization';
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  times: any;
+
+  constructor(private http: HttpClient){}
+
+  getTime(){
+    this.times = this.http.get(this.ROOT_URL + '/posts')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TimeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
