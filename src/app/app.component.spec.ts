759533e85d90fa8ccae53b75456e8be9fafa1b12
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-22 14:08:11 +0100

    minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { TimeComponent } from './components/time/time.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from "./app.component"
+import {MockComponent} from "ng2-mock-component"
+
+// import { AppComponent } from './app.component';
+// import { TimeComponent } from './components/time/time.component';
+// import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        TimeComponent
+        MockComponent({selector: "router-outlet"})
       ],
-      imports: [
-        HttpClientModule
-      ]
+      // imports: [
+      //   HttpClientModule
+      // ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
