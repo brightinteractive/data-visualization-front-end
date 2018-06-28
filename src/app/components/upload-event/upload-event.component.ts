013a40c2efd52f3aff1763338cbdf2e140280da9
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-28 17:28:42 +0100

    working on upload event component

--- src/app/components/upload-event/upload-event.component.ts
+++ src/app/components/upload-event/upload-event.component.ts
@@ -10,6 +10,10 @@ import { Event } from '../../models/event';
 export class UploadEventComponent implements OnInit {
 
   eventModel = new Event();
+  startDateModel = new Date();
+  endDateModel = new Date();
+
+  noOfUploads: number = 10;
 
   @ViewChild("eventForm") eventForm;
   @ViewChild("userIdField") userIdInput : ElementRef;
@@ -20,9 +24,28 @@ export class UploadEventComponent implements OnInit {
   }
 
   onSubmit() {
+    let differenceInDays = this.calculateDifferenceInDays();
+    //Iterate through number of days, iterate through number of Daily Uploads
+    // for( let i=0; i < differenceInDays; i++){
+    //   for( let j=0; i < this.noOfUploads; j++){
+        //generateRandomTime();
+        //change time of event to generated time;
+        //uploadEvent
+    //   }
+    // }
+    
     this.uploadEventService.postEvent(this.eventModel).subscribe();
     this.eventForm.reset();
     this.userIdInput.nativeElement.focus();
+    
+  }
+ 
+  calculateDifferenceInDays() : number{
+    let d1 = new Date(this.startDateModel);
+    let d2 = new Date(this.endDateModel);
+    let timeDiff = Math.abs(d2.getTime()-d1.getTime());
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays + 1;
   }
 
 }
