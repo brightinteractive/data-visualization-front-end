5b2cb28abf55ec3fbdd7459d5be805c949fb8474
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-28 13:56:20 +0100

    codeChanges

--- src/app/components/upload-event/upload-event.component.ts
+++ src/app/components/upload-event/upload-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UploadEventService } from '../../services/upload-event/upload-event.service';
 import { Event } from '../../models/event';
 
@@ -11,14 +11,18 @@ export class UploadEventComponent implements OnInit {
 
   eventModel = new Event('', '', '', 0, '');
 
+  @ViewChild("eventForm") eventForm;
+  @ViewChild("userIdField") userIdInput : ElementRef;
+
   constructor(private uploadEventService: UploadEventService) { }
 
   ngOnInit() {
   }
 
-  uploadEvent() {
+  onSubmit() {
     this.uploadEventService.postEvent(this.eventModel).subscribe();
-    document.getElementById("userId").focus();
+    this.eventForm.reset();
+    this.userIdInput.nativeElement.focus();
   }
 
 }
--- src/app/services/upload-event/upload-event.service.ts
+++ src/app/services/upload-event/upload-event.service.ts
@@ -8,7 +8,6 @@ export class UploadEventService {
   constructor(private http: HttpClient) { }
 
   postEvent(event) {
-    console.log(event);
     return this.http.post<Event>(AppSettings.API_ENDPOINT + '/upload-event', event);
   }
 
