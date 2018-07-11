f9915e328deb2f8609d3be217290055aa802656a
Will Glasse <wg15313@my.bristol.ac.uk>
2018-07-11 16:46:27 +0100

    routing from server to client

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/data-visualization'));
+app.use(express.static( __dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
+//
+// res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+// });
 
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/visualizer', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/simulator', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
