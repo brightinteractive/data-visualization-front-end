186187bfd1de4fa028db331b56fe28bcabfa5b85
Will Glasse <wg15313@my.bristol.ac.uk>
2018-07-11 16:56:37 +0100

    More Heroku tests

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var app = express();
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/data-visualization'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/index.html'));
+app.use(express.static(__dirname + '/dist'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/visualizer', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/simulator', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
