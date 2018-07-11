28ab3e9caeea787859c6becdf1c7fbfc8fb8a914
Will Glasse <wg15313@my.bristol.ac.uk>
2018-07-11 17:34:15 +0100

    Routing now works

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static('./dist/data-visualization'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/data-visualization/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
