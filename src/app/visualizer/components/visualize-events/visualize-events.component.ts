9d4e161f488e649d2da107db6629d7074d09dc51
Will Glasse <wg15313@my.bristol.ac.uk>
2018-07-12 15:34:41 +0100

    simple chart.js example

--- src/app/visualizer/components/visualize-events/visualize-events.component.ts
+++ src/app/visualizer/components/visualize-events/visualize-events.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {VisualizeEventsService} from '../../services/visualize-events.service';
-// import "rxjs/add/operator/take";
-import {take, tap} from 'rxjs/operators';
+import {take} from 'rxjs/operators';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CustomValidators} from '../../../shared/validators/customValidators';
+import {Chart} from 'chart.js';
 
 
 @Component({
@@ -14,7 +14,6 @@ import {CustomValidators} from '../../../shared/validators/customValidators';
 export class VisualizeEventsComponent implements OnInit {
 
   events: Event[];
-
   eventForm: FormGroup;
 
 
@@ -43,6 +42,38 @@ export class VisualizeEventsComponent implements OnInit {
     this.visualizeEventService.getEventsBetweenDateRange(startDate.getTime(), endDate.getTime())
       .pipe(take(1))
       .subscribe(value => this.events = value);
+    this.generateChart();
+  }
+
+
+
+
+  private generateChart() {
+    var canvas = <HTMLCanvasElement> document.getElementById("myChart");
+    var ctx = canvas.getContext("2d");
+
+    var chart = new Chart(ctx, {
+      type: 'bar',
+
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+          label: "Upload",
+          backgroundColor: 'rgb(184, 226, 230)',
+          hoverBackgroundColor: 'rgb(42,176,190)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'day'
+            }
+          }]
+        }
+      }
+    });
   }
 
   shouldShowFieldValidationMessage(fieldname: string): boolean {
